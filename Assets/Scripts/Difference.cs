f662b7554ca3fb0d15547fad8eb87be3e282c1ea
Artem Labutin <artemkasan83@gmail.com>
2020-06-14 09:56:22 +0200

    Base part implemented

---
+++ Assets/Scripts/Difference.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Difference : MonoBehaviour
+{
+    public int FoundItemIndex;
+
+    public GameObject OppositeDifference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null) {
+                var collider = this.GetComponent<CircleCollider2D>();
+                if(hit.collider == collider)
+                {
+                    SelectDifference();
+                }
+            }
+        }
+    }
+
+    public void SelectDifference()
+    {
+        Debug.Log("Something was clicked!");
+        var ownAnimator = GetComponent<Animator>();
+        ownAnimator.SetBool("Selected", true);
+        var oppositeAnimator = OppositeDifference.GetComponent<Animator>();
+        oppositeAnimator.SetBool("Selected", true);
+        Game.AddFoundItems(FoundItemIndex);
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public Text FoundItemsText;
+
+    private static HashSet<int> foundItems = new HashSet<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foundItems.Clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FoundItemsText.text = foundItems.Count.ToString();
+    }
+
+    public static void AddFoundItems(int itemIndex)
+    {
+        foundItems.Add(itemIndex);
+    }
+}
