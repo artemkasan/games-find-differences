cf66af33f6f22f2aa082de80b2347eb3841f9537
Artem Labutin <artemkasan83@gmail.com>
2020-06-17 22:41:08 +0200

    One scene completed

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    public bool MaintainWidth = true;
+    [Range(-1, 1)]
+    public int AdaptPosition;
+
+    private float defaultWidth;
+    private float defaultHeight;
+    private Vector3 cameraPos;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPos = Camera.main.transform.position;
+
+        defaultHeight = Camera.main.orthographicSize;
+        defaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        Debug.Log($"Height: {defaultHeight}, Width: {defaultWidth}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MaintainWidth)
+        {
+            Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+            Camera.main.transform.position = new Vector3(
+                cameraPos.x,
+                AdaptPosition * (defaultWidth - Camera.main.orthographicSize),
+                cameraPos.z);
+        }
+        else
+        {
+            Camera.main.transform.position = new Vector3(
+                AdaptPosition * (defaultWidth - Camera.main.orthographicSize),
+                cameraPos.y,
+                cameraPos.z);
+        }
+    }
+}
--- Assets/Scripts/Difference.cs
+++ Assets/Scripts/Difference.cs
@@ -11,20 +11,21 @@ public class Difference : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null) {
+            if (hit.collider != null)
+            {
                 var collider = this.GetComponent<CircleCollider2D>();
-                if(hit.collider == collider)
+                if (hit.collider == collider)
                 {
                     SelectDifference();
                 }
