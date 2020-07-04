5dff3abedb9d3712641643f4db16ba6836574753
Artem Labutin <artemkasan83@gmail.com>
2020-07-04 12:58:37 +0200

    Implemented start menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+	public Animator transition;
+
+	public float transitionTime;
+
+	public void SelectScene(string sceneName)
+	{
+		StartCoroutine(LoadScene(sceneName));
+	}
+
+	public IEnumerator LoadScene(string sceneName)
+	{
+		transition.SetTrigger("Start");
+		yield return new WaitForSeconds(transitionTime);
+
+		SceneManager.LoadScene(sceneName);
+	}
+}
