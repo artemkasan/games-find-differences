ad740d4db164994c6617bff0d0f6abdbf034a7ce
Artem Labutin <artemkasan83@gmail.com>
2021-01-06 00:04:12 +0100

    fixed images

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+public class NewBehaviourScript
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    public static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles(
+            "Assets/AssetBundles",
+            BuildAssetBundleOptions.None,
+            BuildTarget.WebGL);
+    }
+}
